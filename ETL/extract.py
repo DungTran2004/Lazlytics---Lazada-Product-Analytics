4daadd0f9a4e4b6ed6f42d124a3b86af61d2f7ac
DungTran2004 <tranminhdung200904@gmail.com>
2025-09-10 07:36:48 +0700

    update

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,2 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def get_category_url(): # general lazada url -> specific categories url -> product data
+    url='https://www.lazada.vn/#hp-categories'
+    headers={'User_Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0',
+             'Referer':'https://www.lazada.vn/'}
+    response=requests.get(url=url,headers=headers)
+    bs=BeautifulSoup(response.content,'html.parser')
+    categories_url=bs.find_all('a',class_='pc-custom-link card-categories-li hp-mod-card-hover')
+    try:
+        file_location='C:/MY_PROJECT/Lazlytics---Lazada Product Analytics/ETL/categories_url.txt'
+        with open(file_location,'r',encoding='utf-8') as f:
+                items = set(line.strip() for line in f)
+    except FileNotFoundError:
+        items = set()
+    for x in categories_url:
+        href=x['href']
+        if href not in items:
+            with open(file_location, "a", encoding="utf-8") as f:
+                f.write(href + "\n")
+        else:
+            pass
+    return
+
 def crawling():
     return
