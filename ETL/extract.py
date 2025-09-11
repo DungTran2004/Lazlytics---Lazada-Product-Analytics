309de5ce90069d40ac8286f3e39ca5f72e4dfebc
DungTran2004 <tranminhdung200904@gmail.com>
2025-09-11 08:35:54 +0700

    update

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import random
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+import time
+
 
 
 def get_category_url(): # general lazada url -> specific categories url -> product data
@@ -25,16 +30,58 @@ def get_category_url(): # general lazada url -> specific categories url -> produ
             pass
     return
 
+
+
+
 def crawling():
-    file_location='C:/MY_PROJECT/Lazlytics---Lazada Product Analytics/ETL/categories_url.txt'
-    with open(file_location,'r',encoding='utf-8') as f:
+    file_location = 'C:/MY_PROJECT/Lazlytics---Lazada Product Analytics/ETL/categories_url.txt'
+    session = requests.Session()
+
+    with open(file_location, 'r', encoding='utf-8') as f:
         for line in f:
             if line.startswith('//'):
-                line='https:'+line
-            response=requests.get(line.strip(),params={'ajax':'true'})
-            # data=response.json()
-            print(response.headers.get("Content-Type"))
-            
-    return
+                line = 'https:' + line.strip()
+                
+                headers = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0",
+                    "Referer": "https://www.lazada.vn/",
+                    "Accept": "application/json, text/javascript, */*; q=0.01",
+                    "X-Requested-With": "XMLHttpRequest"
+                }
 
-crawling()
+                response = session.get(line, params={"ajax": "true"}, headers=headers, timeout=15)
+
+                if "application/json" in response.headers.get("Content-Type", ""):
+                    print("Got JSON:", line)
+                    listitem=response.json().get('listItems')
+                    # TODO: datalake
+                    
+                else:
+                    print("Punish, thử với Selenium...")
+
+
+                    edge_options = Options()
+                    edge_options.add_argument("--headless")
+                    driver = webdriver.Edge(options=edge_options)
+                    driver.get("https://www.lazada.vn/")
+
+                    cookies = driver.get_cookies()
+                    driver.quit()
+
+                    cookie_str = "; ".join([f"{c['name']}={c['value']}" for c in cookies])
+                    headers["Cookie"] = cookie_str
+
+                    response = session.get(line, params={"ajax": "true"}, headers=headers, timeout=15)
+
+                    if "application/json" in response.headers.get("Content-Type", ""):
+                        print("Got JSON after Selenium:", line)
+                        listitem=response.json().get('listItems')
+                        # TODO: Lưu vào datalake
+                    else:
+                        print("STILL BLOCKED:", line)
+
+
+                delay = random.uniform(10, 20)
+                print(f"Sleep {delay:.2f} seconds...")
+                time.sleep(delay)
+
---
+++ ETL/keys.py
@@ -0,0 +1,4 @@
+datalake_key={'keyID':'0053440271869d90000000001',
+              'keyName':'Lazlytics-Key',
+              'applicationKey':'K005G4hQLCRZQM9QBYi632QEKG12d7o'}
+datawarehouse_key={}
