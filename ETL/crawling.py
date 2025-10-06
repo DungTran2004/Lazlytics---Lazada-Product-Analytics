f468f26474e0a1482c73279b2ba5adf15a94408f
DungTran2004 <tranminhdung200904@gmail.com>
2025-10-06 22:05:09 +0700

    update

--- ETL/crawling.py
+++ ETL/crawling.py
@@ -1,85 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 import random
-from selenium import webdriver
-from selenium.webdriver.edge.service import Service
 import time
-import boto3
-from botocore.client import Config
-from keys import *
-import os
-from datetime import datetime
-
-key_id=datalake_key['keyID']
-key_name=datalake_key['keyName']
-application_key=datalake_key['applicationKey']
-endpoint=datalake_key['Endpoint']
-
-
-def current_date():
-    today=datetime.now()
-    return f'{today.year}-{today.month}-{today.day}'
-
-
-def create_current_date_file():
-    today = datetime.now()
-    date_format = today.strftime("%Y-%m-%d")  
-    folder = "./Lazada"
-    os.makedirs(folder, exist_ok=True)       
-    
-    file_path = f"{folder}/{date_format}.json"
-    
-    if not os.path.exists(file_path):
-        with open(file_path, "w", encoding="utf-8") as f:
-            pass  
-    
-    return file_path
-    
-
-
-def append_to_json(new_data, file_path):
-    """Thêm dữ liệu vào file JSON dạng list"""
-    if os.path.exists(file_path):
-        with open(file_path, 'r', encoding='utf-8') as f:
-            try:
-                data = json.load(f)
-            except json.JSONDecodeError:
-                data = []
-    else:
-        data = []
-
-    # Nếu new_data là list thì nối, còn không thì append
-    if isinstance(new_data, list):
-        data.extend(new_data)
-    else:
-        data.append(new_data)
-
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-        
-  
-  
-        
-def get_cookie():
-    edge_options = webdriver.EdgeOptions()
-    edge_options.add_argument("--headless=new")  # nếu muốn chạy ẩn
-    edge_options.add_argument("--disable-gpu")
-    edge_options.add_argument("--no-sandbox")
-
-    service = Service(r"C:/MY_PROJECT/Lazlytics---Lazada Product Analytics/edgedriver_win64/msedgedriver.exe")
-    driver = webdriver.Edge(service=service, options=edge_options)
-    
-    driver.get("https://www.lazada.vn/")
-    cookie = "; ".join([f"{c['name']}={c['value']}" for c in driver.get_cookies()])
-    driver.quit()
-    return cookie
-
-
+from untils import *
 
 def get_category_url(): # general lazada url -> specific categories url -> product data
     url='https://www.lazada.vn/#hp-categories'
-    headers={'User_Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0',
+    headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0',
              'Referer':'https://www.lazada.vn/'}
     response=requests.get(url=url,headers=headers)
     bs=BeautifulSoup(response.content,'html.parser')
@@ -162,42 +89,11 @@ def crawling():
                 else:
                     date_file=create_current_date_file()
                     
-                    append_to_json(listitem, date_file)
+                    save_data(listitem, date_file)
             
-            break
                     
             delay = random.uniform(10, 20)
             
             print(f"Sleep {delay:.2f} seconds...")
             
             time.sleep(delay)
-
-
-
-
-
-def load_datalake_layer1(local_dir):
-    s3=boto3.client(
-    "s3",
-    endpoint_url=endpoint,
-    aws_access_key_id=key_id,
-    aws_secret_access_key=application_key,
-    config=Config(signature_version="s3v4")
-    )
-    bucket_name='Lazlytics-DataLake'
-    for filename in os.listdir(local_dir):
-        local_path = os.path.join(local_dir, filename)
-        object_key = f"RawData/Lazada/2025-09-11/{filename}"
-        s3.upload_file(local_path, bucket_name, object_key)
-        print(f"✅ Uploaded {filename} → {object_key}")
-    return
-
-def load_datalake_layer2():
-    return
-
-def load_datalake_layer3():
-    return
-
-
-if __name__=='__main__':
-    crawling()
---
+++ ETL/untils.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+import os 
+import json
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+
+
+def current_date():
+    today=datetime.now()
+    return f'{today.year}-{today.month}-{today.day}'
+
+
+def create_current_date_file():
+    today = datetime.now()
+    date_format = today.strftime("%Y-%m-%d")  
+    folder = "C:/MY_PROJECT/Lazlytics---Lazada Product Analytics/ETL/RawData/Lazada"
+    os.makedirs(folder, exist_ok=True)       
+    
+    file_path = f"{folder}/{date_format}.json"
+    
+    if not os.path.exists(file_path):
+        with open(file_path, "w", encoding="utf-8") as f:
+            pass  
+    
+    return file_path
+
+
+
+def save_data(new_data, file_path):
+    """Thêm dữ liệu vào file JSON dạng list"""
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                data = []
+    else:
+        data = []
+
+    if isinstance(new_data, list):
+        data.extend(new_data)
+    else:
+        data.append(new_data)
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+        
+        
+        
+def get_cookie():
+    edge_options = webdriver.EdgeOptions()
+    edge_options.add_argument("--headless=new")  # nếu muốn chạy ẩn
+    edge_options.add_argument("--disable-gpu")
+    edge_options.add_argument("--no-sandbox")
+
+    service = Service(r"C:/MY_PROJECT/Lazlytics---Lazada Product Analytics/edgedriver_win64/msedgedriver.exe")
+    driver = webdriver.Edge(service=service, options=edge_options)
+    
+    driver.get("https://www.lazada.vn/")
+    cookie = "; ".join([f"{c['name']}={c['value']}" for c in driver.get_cookies()])
+    driver.quit()
+    return cookie
